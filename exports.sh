ea78e106dfae2951f6bba9da0896a30643479fc8
Nic DeVos <nic@goodrx.com>
2018-10-03 12:19:44 -0700

    Add color magenta

--- exports.sh
+++ exports.sh
@@ -5,6 +5,7 @@
 export COLOR_BLUE="$(tput setaf 4)"
 export COLOR_CYAN="$(tput setaf 6)"
 export COLOR_GREEN="$(tput setaf 2)"
+export COLOR_MAGENTA="$(tput setaf 5)"
 export COLOR_RED="$(tput setaf 1)"
 export COLOR_RESET="$(tput sgr0)"
 export COLOR_WHITE="$(tput setaf 7)"
--- prompt.sh
+++ prompt.sh
@@ -2,4 +2,4 @@
 # prompt
 #
 
-export PS1="[\[$COLOR_CYAN\]\u@\h\[$COLOR_RESET\]:\[$COLOR_YELLOW\]\w\[$COLOR_RESET\]]\$(__git_prompt)\n\$ "
+export PS1="[\[$COLOR_MAGENTA\]\u\[$COLOR_RESET\]@\[$COLOR_CYAN\]\h\[$COLOR_RESET\]:\[$COLOR_YELLOW\]\w\[$COLOR_RESET\]]\$(__git_prompt)\n\$ "
